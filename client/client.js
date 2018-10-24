a4792846370139ff0061341616a35157b9565e9a
Husam Lahm <37015049+ThreePalmTrees@users.noreply.github.com>
2018-10-24 20:44:11 +0200

    added npm package v1.0.0

---
+++ client/client.js
@@ -0,0 +1,9 @@
+const hackYourForecast = require("hack-your-forecast");
+
+const cities = [
+  { lat: 37.5681, lng: 126.998, timestamp: 1539268428 },
+  { lat: 35.9581, lng: 128.936, timestamp: new Date() },
+  { lat: 35.9581, lng: "128.936a" }
+];
+
+hackYourForecast.lookup(cities).then(console.log);
